d8fb36361ab444413d28162b0e484b91827dff15
VuongTien294 <vuongtien2942000@gmail>
2022-04-24 15:43:52 +0700

    Thêm api product

--- demo/src/main/java/com/doantotnghiep/demo/controller/admin/AdminSizeController.java
+++ demo/src/main/java/com/doantotnghiep/demo/controller/admin/AdminSizeController.java
@@ -3,6 +3,7 @@ package com.doantotnghiep.demo.controller.admin;
 import com.doantotnghiep.demo.dto.request.admin.AddSizeRequest;
 import com.doantotnghiep.demo.dto.response.admin.SizeDetailResponse;
 import com.doantotnghiep.demo.service.SizeService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import java.util.List;
 public class AdminSizeController {
     private final SizeService sizeService;
 
+    @ApiOperation("Api cho admin tạo mới 1 size cho product")
     @PostMapping("/admin/size/add")
     public void addSize(
             @RequestBody(required = true) AddSizeRequest addSizeRequest
@@ -37,6 +39,7 @@ public class AdminSizeController {
 //        sizeService.modifiedSize(addSizeRequest);
 //    }
 
+    @ApiOperation("Api cho admin xóa 1 size của product")
     @GetMapping("/admin/size/delete")
     public void deleteSize(
             @RequestParam(required = true) Long id
