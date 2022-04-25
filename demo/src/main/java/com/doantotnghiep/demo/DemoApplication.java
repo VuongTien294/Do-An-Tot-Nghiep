d15e4d51cc1c1335170b7108a46834d85afe3ea0
Vuong Tien <>
2022-04-25 16:23:51 +0700

    dfhgdh

--- demo/src/main/java/com/doantotnghiep/demo/DemoApplication.java
+++ demo/src/main/java/com/doantotnghiep/demo/DemoApplication.java
@@ -10,17 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
-
-		// Set Cloudinary instance
-//		Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
-//				"cloud_name", "xxx", // insert here you cloud name
-//				"api_key", "xxx", // insert here your api code
-//				"api_secret", "xxx")); // insert here your api secret
-//		SingletonManager manager = new SingletonManager();
-//		manager.setCloudinary(cloudinary);
-//		manager.init();
-
-
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
